37e303334d7adcb955853c6c350f9619f9637fa1
raulmanzas <raulmanzas@gmail.com>
2020-04-11 12:31:29 -0300

    adds line to start the cluster

--- redis/build_cluster.sh
+++ redis/build_cluster.sh
@@ -9,13 +9,23 @@ docker network inspect $CLUSTER_NETWORK >/dev/null 2>&1 || docker network create
 # downloads the official image
 docker pull redis
 
+declare -a IP_LIST
 # starts the container for each node in the cluster
 for ((i=0; i<$NUMBER_OF_NODES; ++i))
 do
-    echo "Creating node ${i}..."
+    NAME=$NODE_NAME_PREFIX$i
+    echo "Creating node ${NAME}..."
+    
     docker run -d \
         -v $NODE_CONFIG_FILE_PATH:$REDIS_CONFIG_FILE_PATH \
-        --name $NODE_NAME_PREFIX$i \
+        --name $NAME \
         --net $CLUSTER_NETWORK \
         redis redis-server $REDIS_CONFIG_FILE_PATH
+    
+    # Captures the IP of the node
+    NODE_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $NAME)
+    IP_LIST[$i]=$NODE_IP:$REDIS_PORT
 done
+
+# starts the cluster itself, with 1 replica node for each master (by shard)
+docker run -i --rm --net $CLUSTER_NETWORK redis redis-cli --cluster create ${IP_LIST[*]} --cluster-replicas 1
--- redis/config.sh
+++ redis/config.sh
@@ -4,3 +4,4 @@ NODE_NAME_PREFIX=redis_node
 CLUSTER_NETWORK=cluster
 NODE_CONFIG_FILE_PATH=cluster-node.conf
 REDIS_CONFIG_FILE_PATH=/usr/local/etc/redis/redis.conf
+REDIS_PORT=6379
